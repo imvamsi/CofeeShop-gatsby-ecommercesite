3664165af9f7f4a91d7cce56b72bbc0a9e74a127
imvamsi <krrishvamsi7@gmail.com>
2019-11-16 13:18:23 -0500

    added navbar and some basic styling :construction:

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,9 +6,11 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import "bootstrap/dist/css/bootstrap.css"
 import { FaGulp } from "react-icons/fa"
+import Navbar from "../components/Globals/Navbar"
 
 const IndexPage = () => (
   <Layout>
+    <Navbar />
     <SEO title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
