f40c756e73b0216c89a9cec9e4fad7bff0a6022a
imvamsi <krrishvamsi7@gmail.com>
2019-11-16 14:27:14 -0500

    added background image :beer:

---
+++ src/components/Globals/BackgroundSection.js
@@ -0,0 +1,28 @@
+import React from "react"
+import BackgroundImage from "gatsby-background-image"
+
+export default function BackgroundSection({
+  img,
+  styleClass,
+  title,
+
+  children,
+}) {
+  return (
+    <BackgroundImage className={styleClass} fluid={img}>
+      <div>
+        <h1
+          className="title text-white text-uppercase
+       text-center display-4 font-weight-bold"
+        >
+          {title}
+        </h1>
+        {children}
+      </div>
+    </BackgroundImage>
+  )
+}
+BackgroundSection.defaultProps = {
+  title: "Default Title",
+  styleClass: "defaultBackground",
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 
@@ -7,15 +7,32 @@ import SEO from "../components/seo"
 import "bootstrap/dist/css/bootstrap.css"
 import { FaGulp } from "react-icons/fa"
 import Navbar from "../components/Globals/Navbar"
+import BackgroundSection from "../components/Globals/BackgroundSection"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <Navbar />
+    <BackgroundSection
+      img={data.img.childImageSharp.fluid}
+      title="regular joe's"
+    />
     <SEO title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <FaGulp />
   </Layout>
 )
+
+export const query = graphql`
+  {
+    img: file(relativePath: { eq: "default-background.jpeg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
