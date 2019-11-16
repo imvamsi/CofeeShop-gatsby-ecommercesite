6a774c7938f6155e7f2b5d4c7d44f220e2e69859
imvamsi <krrishvamsi7@gmail.com>
2019-11-16 15:11:28 -0500

    added footer component and about page :construction:

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,11 +8,18 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-
+import Navbar from "../components/Globals/Navbar"
+import Footer from "../components/Globals/Footer"
 import Header from "./header"
 import "./layout.css"
 
-const Layout = ({ children }) => <>{children}</>
+const Layout = ({ children }) => (
+  <>
+    <Navbar />
+    {children}
+    <Footer />
+  </>
+)
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
--- src/pages/about.js
+++ src/pages/about.js
@@ -10,12 +10,13 @@ import { FaGulp } from "react-icons/fa"
 import BackgroundSection from "../components/Globals/BackgroundSection"
 import Info from "../components/Home/Info"
 
-const IndexPage = ({ data }) => (
+const AboutPage = ({ data }) => (
   <Layout>
     {/* <Navbar /> */}
     <BackgroundSection
       img={data.img.childImageSharp.fluid}
-      title="regular joe's"
+      title="About Us"
+      styleClass="about-background"
     />
     <Info />
   </Layout>
@@ -23,7 +24,7 @@ const IndexPage = ({ data }) => (
 
 export const query = graphql`
   {
-    img: file(relativePath: { eq: "default-background.jpeg" }) {
+    img: file(relativePath: { eq: "about-background.jpeg" }) {
       childImageSharp {
         fluid {
           ...GatsbyImageSharpFluid_tracedSVG
@@ -33,4 +34,4 @@ export const query = graphql`
   }
 `
 
-export default IndexPage
+export default AboutPage
